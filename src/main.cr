0e874014baa07a4001da2f24058a9588fff3699c
UberNihilist <dx98171@gmail.com>
2021-11-24 22:05:42 +0200

    new option added

--- src/main.cr
+++ src/main.cr
@@ -2,7 +2,7 @@ require "funny_wrds"
 require "option_parser"
 
 print "Enter path to json file: "
-funny_words = FunnyWords.new(gets.to_s)
+funny_words = FW::FunnyWords.new(gets.to_s)
 
 OptionParser.parse do |parser|
     parser.banner = "Welcome to the Funny Words"
@@ -16,9 +16,9 @@ OptionParser.parse do |parser|
         puts funny_words.get_words(num.to_i)  
     end
 
-    #parser.on("-c", "--concatenated-words") do |num|
-    #    puts funny_words.get_concatenated_words
-    #end
+    parser.on("-c", "--concatenated-words") do |num|
+        puts funny_words.get_concatenated_words
+    end
 
     parser.invalid_option do |flag|
         STDERR.puts "ERROR: #{flag} is not a valid option."
