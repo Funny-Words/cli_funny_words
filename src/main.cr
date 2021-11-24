a8f5721b4dba12454fac1589fe123c51dedb5072
UberNihilist <dx98171@gmail.com>
2021-11-24 22:00:29 +0200

    shards doesnt words............

--- src/main.cr
+++ src/main.cr
@@ -1,6 +1,9 @@
 require "funny_wrds"
 require "option_parser"
 
+print "Enter path to json file: "
+funny_words = FunnyWords.new(gets.to_s)
+
 OptionParser.parse do |parser|
     parser.banner = "Welcome to the Funny Words"
 
@@ -10,10 +13,12 @@ OptionParser.parse do |parser|
     end
 
     parser.on("-g NUM", "--get=NUM") do |num| 
-        print "Enter path to json file: "
-        funny_words = FunnyWords.new(gets.to_s) # path to name.json that contains words 
-        puts funny_words.get_words(num.to_i) 
-    end # NUM is a number of words
+        puts funny_words.get_words(num.to_i)  
+    end
+
+    #parser.on("-c", "--concatenated-words") do |num|
+    #    puts funny_words.get_concatenated_words
+    #end
 
     parser.invalid_option do |flag|
         STDERR.puts "ERROR: #{flag} is not a valid option."
