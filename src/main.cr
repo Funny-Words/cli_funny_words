7aafb79f8e25291ab00e8a33a3ade0e19dd7c4ac
UberNihilist <dx98171@gmail.com>
2021-12-16 18:56:43 +0200

    new update

--- src/main.cr
+++ src/main.cr
@@ -1,24 +1,21 @@
 require "funny_wrds"
 require "option_parser"
 
-print "Enter path to json file: "
-funny_words = FunnyWords.new gets.to_s
+def get_file(path = "../funny_words_json/words.json")
+    FunnyWords.new path
+end
 
 OptionParser.parse do |parser|
+    print "Enter path to json file: "
+    funny_words = get_file(gets.not_nil!.to_s)
+
     parser.banner = "Welcome to the Funny Words"
 
-    parser.on("-h", "--help") do 
-        puts parser
-        exit
-    end
+    parser.on("-h", "--help") { puts parser; exit }
 
-    parser.on("-g NUM", "--get=NUM") do |num| 
-        puts funny_words.get_words(num.to_i)  
-    end
+    parser.on("-g NUM", "--get=NUM") { |num| puts funny_words.get_words(num.to_i) }
     
-    parser.on("-c", "--concatenated-words") do |num|
-        puts funny_words.get_concatenated_words
-    end
+    parser.on("-c", "--concatenated-words") { |num| puts funny_words.get_concatenated_words }
 
     parser.invalid_option do |flag|
         STDERR.puts "ERROR: #{flag} is not a valid option."
