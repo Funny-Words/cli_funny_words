d3abbcfc0d7eeac4ca6633d4c51cafd98c76c05a
UberNihilist <dx98171@gmail.com>
2022-02-05 15:20:22 +0200

    refactoring commit

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Simple light-weight CLI program that writes in console funny words.
 Download the latest release from Releases or compile it yourself.
 
 ```bash
-git clone https://github.com/UberNihilist/cli_funny_words.git
+git clone git@github.com:fragilecopy/cli_funny_words.git
 ```
 
 If you have downloaded Crystal then do:
@@ -32,20 +32,9 @@ crystal build path --release
 
 Then enter path to **words.json**
 
-## Development
-
-* General guides
-  * Use **double** quotes
-
-* JSON guides
-  * Try not to add words that exists
-
-* Code guides
-  * Use `block` syntax instead of `do...end` if it's possible
-
 ## Contributing
 
-1. Fork it (<https://github.com/UberNihilist/cli_funny_words/fork>)
+1. Fork it (<https://github.com/fragilecopy/cli_funny_words/fork>)
 2. Create your feature branch (`git checkout -b my-new-feature`)
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
--- src/main.cr
+++ src/main.cr
@@ -2,26 +2,26 @@ require "funny_wrds"
 require "option_parser"
 
 OptionParser.parse do |parser|
-    print "Enter path to json file: "
+  print "Enter path to json file: "
 
-    begin 
-        funny_words = FunnyWords.new gets.not_nil!.to_s
-    rescue err
-        STDERR.puts err.message 
-        exit 1
-    end
+  begin
+    funny_words = FunnyWords.new gets.not_nil!.to_s
+  rescue err
+    STDERR.puts err.message
+    exit 1
+  end
 
-    parser.banner = "Welcome to the Funny Words"
+  parser.banner = "Welcome to the Funny Words"
 
-    parser.on("-h", "--help") { puts parser; exit }
+  parser.on("-h", "--help") { puts parser; exit }
 
-    parser.on("-g NUM", "--get=NUM") { |num| puts funny_words.get_words(num.to_i) }
-    
-    parser.on("-c", "--concatenated-words") { |num| puts funny_words.get_concatenated_words }
+  parser.on("-g NUM", "--get=NUM") { |num| puts funny_words.get_words(num.to_i) }
 
-    parser.invalid_option do |flag|
-        STDERR.puts "ERROR: #{flag} is not a valid option."
-        STDERR.puts parser
-        exit 1
-    end
+  parser.on("-c", "--concat-word") { puts funny_words.get_concatenated_words }
+
+  parser.invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    STDERR.puts parser
+    exit 1
+  end
 end
